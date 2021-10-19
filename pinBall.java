7491687ba80b7a5d329379daf7327f4c4d4072cc
Naveen-T <naveentm95@gmail.com>
2021-10-19 21:04:11 +0530

    10 pin

---
+++ pinBall.java
@@ -0,0 +1,13 @@
+public static void main(String arg[]){
+Scanner sc = new Scanner(System.in);
+int i=0, sum=0, one=0, two=0;
+char x[10], y[10];
+
+for(i=0; i<=10; i++)
+System.out.println("Enter two rolls for single frame");
+char[one++] = sc.nextLine();
+char[two++] = sc.nextLine();
+sum = a[one] + b[two];
+if(a[])
+{}
+}
