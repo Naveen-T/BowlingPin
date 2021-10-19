6489d460060cd32ecb29817817a630b42a671697
Naveen-T <naveentm95@gmail.com>
2021-10-19 22:43:22 +0530

    updated

--- pinBall.java
+++ pinBall.java
@@ -1,12 +1,13 @@
 public static void main(String arg[]){
 Scanner sc = new Scanner(System.in);
 int i=0, sum=0, one=0, two=0, spare = 0, strike = 0;
-char a[10], b[10];
+char a[] = new char[];
+char b[] = new char[];
 
 for(i=0; i<10; i++){
     Syste.out.println("Enter two rolls for single frame");
-    char[] a = sc.next().toCharArray();
-    char[] b = sc.next().toCharArray();
+     a[one] = sc.next().charAt(one);
+     b[two] = sc.next().charAt(two);
       
             if(a[one] == 'X'){
               sum = sum + 10;
@@ -22,15 +23,15 @@ for(i=0; i<10; i++){
               bonus = 2;
             }
             else
-               sum = a[one] + b[two];
+               sum = Integer.parseInt(String.valueOf(a[one]) + Integer.parseInt(String.valueOf(b[two]);
           
        if(bonus == 1){
-          strike = a[one] + b[two];
+          strike = Integer.parseInt(String.valueOf(a[one]) + Integer.parseInt(String.valueOf(b[two]);
           sum = sum + strike;
          bonus = 0;
         }
         else if(bonus == 2){
-          spare = a[one];
+          spare = Integer.parseInt(String.valueOf(a[one]);
           sum = sum + spare;
           bonus = 0;
         }
