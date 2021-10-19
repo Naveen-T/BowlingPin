8d025a7f36e0952a5f5eaf3a999f33ab80a59505
Naveen-T <naveentm95@gmail.com>
2021-10-19 22:35:20 +0530

    10 pin code

--- pinBall.java
+++ pinBall.java
@@ -1,13 +1,40 @@
 public static void main(String arg[]){
 Scanner sc = new Scanner(System.in);
-int i=0, sum=0, one=0, two=0;
-char x[10], y[10];
+int i=0, sum=0, one=0, two=0, spare = 0, strike = 0;
+char a[10], b[10];
 
-for(i=0; i<=10; i++)
-System.out.println("Enter two rolls for single frame");
-char[one++] = sc.nextLine();
-char[two++] = sc.nextLine();
-sum = a[one] + b[two];
-if(a[])
-{}
+for(i=0; i<10; i++){
+    Syste.out.println("Enter two rolls for single frame");
+    char[] a = sc.next().toCharArray();
+    char[] b = sc.next().toCharArray();
+      
+            if(a[one] == 'X'){
+              sum = sum + 10;
+              bonus = 1;
+              if(i == 0){
+                      one++;
+                      two++;
+                      break;
+              }
+            }
+            else if(b[two] == '/'){
+              sum = sum + 10;
+              bonus = 2;
+            }
+            else
+               sum = a[one] + b[two];
+          
+       if(bonus == 1){
+          strike = a[one] + b[two];
+          sum = sum + strike;
+         bonus = 0;
+        }
+        else if(bonus == 2){
+          spare = a[one];
+          sum = sum + spare;
+          bonus = 0;
+        }
+        one++;
+        two++;
+  }
 }
